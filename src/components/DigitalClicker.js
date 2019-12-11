8215eef9072b89a4eac86eda9db077b4c5b5afbb
Michael Schiumo <mschiumo18@gmail.com>
2019-12-11 14:50:47 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,32 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+import YouTubeDebugger from './YouTubeDebugger.js';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  anotherClick = () => {
+    this.setState(previousState => {
+      return {
+        timesClicked: previousState.timesClicked + 1
+      }
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.anotherClick}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
+
+export default DigitalClicker;
